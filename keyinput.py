4bc83ad3dedb06cb9b80ba3fc3b8907cdf1c3b05
Alloy22 <aloysius22@hotmail.sg>
2019-01-08 21:49:12 +0800

    Update keyinput.py

--- keyinput.py
+++ keyinput.py
@@ -1,1 +1,36 @@
+import sys
+import tty
+import termios
+
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if ch == '0x03':
+        raise KeyboardInterrupt
+    return ch
+
+def readkey(getchar_fn = None):
+    getchar = getchar_fn or readchar
+    c1 = getchar()
+    if ord(c1) != 0x1b:
+        return c1
+    c2 = getchar()
+    if ord(c2) != 0x5b:
+        return c1
+    c3 = getchar()
+    return ord(c3) - 65
+
+def main():
+        return "a"
+
 
